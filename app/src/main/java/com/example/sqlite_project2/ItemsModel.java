de4b5e5ad72bb6347ce940ce1b172833885c4f5c
Sushant Adhikari <sushantadk18@gmail.com>
2025-02-13 08:53:37 +0545

    Add Items

---
+++ app/src/main/java/com/example/sqlite_project2/ItemsModel.java
@@ -0,0 +1,41 @@
+package com.example.sqlite_project2;
+
+import android.net.Uri;
+
+public class ItemsModel {
+    private int id;
+    private String name,description;
+    private Uri image;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Uri getImage() {
+        return image;
+    }
+
+    public void setImage(Uri image) {
+        this.image = image;
+    }
+}
+
